c21c120d34494fd992fef02482fe48b3f152bd61
pabook <pabook.32@gmail.com>
2013-04-01 20:23:26 +0900

    [Opti]Optimized Code :3

---
+++ src/com/github/pabook/commandkepper/CommandKepper.java
@@ -0,0 +1,15 @@
+package com.github.pabook.commandkepper;
+
+import com.google.common.base.FinalizablePhantomReference;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class CommandKepper extends JavaPlugin {
+
+    public void onEnable(){
+
+    }
+
+    public void onDisable(){
+
+    }
+}
---
+++ src/com/github/pabook/commandkepper/getCommand.java
@@ -0,0 +1,7 @@
+package com.github.pabook.commandkepper;
+
+import org.bukkit.event.Listener;
+
+public class getCommand implements Listener {
+
+}
