6c5c81699d092fc47f282411b62bede0f9278789
pabook <pabook.32@gmail.com>
2013-04-01 20:55:38 +0900

    During optimization... 50%

--- src/com/github/pabook/commandkepper/CommandKepper.java
+++ src/com/github/pabook/commandkepper/CommandKepper.java
@@ -1,15 +1,25 @@
 package com.github.pabook.commandkepper;
 
-import com.google.common.base.FinalizablePhantomReference;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.List;
 
 public class CommandKepper extends JavaPlugin {
 
+    public String kickMsg, noticeMsg, noticeStaffMsg;
+    public List<String> kickCmd;
+
+    @Override
     public void onEnable(){
+        getServer().getPluginManager().registerEvents(new getCommand(this), this);
+        getConfig().options().copyDefaults(true);
+        saveDefaultConfig();
+        saveConfig();
+        kickCmd = getConfig().getStringList("KickCommand");
+        kickMsg = getConfig().getString("KickMessage");
+        noticeMsg = getConfig().getString("BroadCastMessage");
+        noticeStaffMsg = getConfig().getString("BroadCastStaffMessage");
 
     }
 
-    public void onDisable(){
-
-    }
 }
--- src/com/github/pabook/commandkepper/getCommand.java
+++ src/com/github/pabook/commandkepper/getCommand.java
@@ -1,7 +1,30 @@
 package com.github.pabook.commandkepper;
 
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 
 public class getCommand implements Listener {
+    CommandKepper cmdKepper;
 
+    public getCommand(CommandKepper cmdkepper){
+        this.cmdKepper = cmdkepper;
+    }
+
+        @EventHandler
+        public void getPlayerCommand(PlayerCommandPreprocessEvent event){
+            Player senderPlayer = event.getPlayer();
+            String getCmd = event.getMessage();
+
+            if(senderPlayer.hasPermission("commandkepper.bypass") || senderPlayer.isOp()){
+                    return;
+            }
+
+            for(int i = 0 ; i < cmdKepper.kickCmd.size() ; i++){
+                    if(cmdKepper.kickCmd.get(i).equalsIgnoreCase(getCmd)){
+                    }
+            }
+        }
 }
+
