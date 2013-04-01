bb82ee0ebf1adf6785930e38c09da824a40e6291
pabook <pabook.32@gmail.com>
2013-04-01 21:04:33 +0900

    During optimization... 80% :3

--- src/com/github/pabook/commandkepper/getCommand.java
+++ src/com/github/pabook/commandkepper/getCommand.java
@@ -1,5 +1,6 @@
 package com.github.pabook.commandkepper;
 
+import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -23,6 +24,14 @@ public class getCommand implements Listener {
 
             for(int i = 0 ; i < cmdKepper.kickCmd.size() ; i++){
                     if(cmdKepper.kickCmd.get(i).equalsIgnoreCase(getCmd)){
+                        Player[] onlinePlayers = cmdKepper.getServer().getOnlinePlayers();
+                        for(int i = 0 ; i < onlinePlayers.length ; i++){
+                            if(onlinePlayers[i].hasPermission("commandkepper.staff") || onlinePlayers[i].isOp()){
+                                onlinePlayers[i].sendMessage("[CommandKepper]" + cmdKepper.noticeStaffMsg.replaceAll("%Player%", ChatColor.GREEN + senderPlayer.getName() + ChatColor.WHITE).replaceAll("%Command%", ChatColor.GRAY + getCmd + ChatColor.WHITE));
+                            }
+                        }
+                        cmdKepper.getServer().broadcastMessage("[CommandKepper]" + cmdKepper.noticeStaffMsg.replaceAll("%Player%", ChatColor.GREEN + senderPlayer.getName() + ChatColor.WHITE).replaceAll("%Command%", ChatColor.GRAY + getCmd + ChatColor.WHITE));
+                        senderPlayer.kickPlayer(cmdKepper.kickMsg);
                     }
             }
         }
