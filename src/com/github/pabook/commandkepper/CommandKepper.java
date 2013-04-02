5434d796018c9a3c840e6e48986a9572188fde42
pabook <pabook.32@gmail.com>
2013-04-02 17:17:27 +0900

    During optimization...Complite!!
    :3

--- src/com/github/pabook/commandkepper/CommandKepper.java
+++ src/com/github/pabook/commandkepper/CommandKepper.java
@@ -3,11 +3,13 @@ package com.github.pabook.commandkepper;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.List;
+import java.util.logging.Logger;
 
 public class CommandKepper extends JavaPlugin {
 
     public String kickMsg, noticeMsg, noticeStaffMsg;
     public List<String> kickCmd;
+    Logger log;
 
     @Override
     public void onEnable(){
@@ -19,7 +21,6 @@ public class CommandKepper extends JavaPlugin {
         kickMsg = getConfig().getString("KickMessage");
         noticeMsg = getConfig().getString("BroadCastMessage");
         noticeStaffMsg = getConfig().getString("BroadCastStaffMessage");
-
     }
 
 }
--- src/com/github/pabook/commandkepper/getCommand.java
+++ src/com/github/pabook/commandkepper/getCommand.java
@@ -25,9 +25,9 @@ public class getCommand implements Listener {
             for(int i = 0 ; i < cmdKepper.kickCmd.size() ; i++){
                     if(cmdKepper.kickCmd.get(i).equalsIgnoreCase(getCmd)){
                         Player[] onlinePlayers = cmdKepper.getServer().getOnlinePlayers();
-                        for(int i = 0 ; i < onlinePlayers.length ; i++){
-                            if(onlinePlayers[i].hasPermission("commandkepper.staff") || onlinePlayers[i].isOp()){
-                                onlinePlayers[i].sendMessage("[CommandKepper]" + cmdKepper.noticeStaffMsg.replaceAll("%Player%", ChatColor.GREEN + senderPlayer.getName() + ChatColor.WHITE).replaceAll("%Command%", ChatColor.GRAY + getCmd + ChatColor.WHITE));
+                        for(int ii = 0 ; ii < onlinePlayers.length ; ii++){
+                            if(onlinePlayers[ii].hasPermission("commandkepper.staff") || onlinePlayers[i].isOp()){
+                                onlinePlayers[ii].sendMessage("[CommandKepper]" + cmdKepper.noticeStaffMsg.replaceAll("%Player%", ChatColor.GREEN + senderPlayer.getName() + ChatColor.WHITE).replaceAll("%Command%", ChatColor.GRAY + getCmd + ChatColor.WHITE));
                             }
                         }
                         cmdKepper.getServer().broadcastMessage("[CommandKepper]" + cmdKepper.noticeStaffMsg.replaceAll("%Player%", ChatColor.GREEN + senderPlayer.getName() + ChatColor.WHITE).replaceAll("%Command%", ChatColor.GRAY + getCmd + ChatColor.WHITE));
